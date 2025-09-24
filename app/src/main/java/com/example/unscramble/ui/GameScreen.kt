20b0b2e4d832d709fb6cafeb6c688b37f4986906
DyablOcOrps <bap.dubois24@gmail.com>
2025-09-24 14:31:06 -0400

    Étape8
    Modification du score et du nombre de mots

--- app/src/main/java/com/example/unscramble/ui/GameScreen.kt
+++ app/src/main/java/com/example/unscramble/ui/GameScreen.kt
@@ -85,6 +85,7 @@ fun GameScreen(
             onKeyboardDone = { gameViewModel.checkUserGuess() },
             currentScrambleWord = gameUiState.currentScrambleWord,
             isGuessWrong = gameUiState.isGuessedWordWrong,
+            wordCount = gameUiState.currentWordCount,
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentHeight()
@@ -109,7 +110,7 @@ fun GameScreen(
             }
 
             OutlinedButton(
-                onClick = { },
+                onClick = { gameViewModel.skipWord()},
                 modifier = Modifier.fillMaxWidth()
             ) {
                 Text(
@@ -119,7 +120,7 @@ fun GameScreen(
             }
         }
 
-        GameStatus(score = 0, modifier = Modifier.padding(20.dp))
+        GameStatus(score = gameUiState.score, modifier = Modifier.padding(20.dp))
     }
 }
 
@@ -143,6 +144,7 @@ fun GameLayout(
     onKeyboardDone: () -> Unit,
     isGuessWrong: Boolean,
     currentScrambleWord: String,
+    wordCount: Int,
     modifier: Modifier = Modifier) {
     val mediumPadding = dimensionResource(R.dimen.padding_medium)
 
@@ -161,7 +163,7 @@ fun GameLayout(
                     .background(colorScheme.surfaceTint)
                     .padding(horizontal = 10.dp, vertical = 4.dp)
                     .align(alignment = Alignment.End),
-                text = stringResource(R.string.word_count, 0),
+                text = stringResource(R.string.word_count, wordCount),
                 style = typography.titleMedium,
                 color = colorScheme.onPrimary
             )
--- app/src/main/java/com/example/unscramble/ui/GameUiState.kt
+++ app/src/main/java/com/example/unscramble/ui/GameUiState.kt
@@ -2,5 +2,7 @@ package com.example.unscramble.ui
 
 data class GameUiState(
     val currentScrambleWord: String = "",
-    val isGuessedWordWrong: Boolean = false
+    val isGuessedWordWrong: Boolean = false,
+    val currentWordCount: Int = 1,
+    val score: Int = 0
 )
